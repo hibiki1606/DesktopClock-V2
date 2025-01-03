657423ae471fd8869b44e6df35be48f3ee250568
ika004 <ry.0422.ry@gmail.com>
2025-01-03 23:12:47 +0900

    V2.1。クロップ機能の追加。 こっそり隠し機能も追加。

--- DesktopClock-V2/Form1.cs
+++ DesktopClock-V2/Form1.cs
@@ -21,9 +21,6 @@ namespace DesktopClock_V2
             InitializeComponent();
         }
 
-
-
-
         private void apdatetime()
         {
             DateTime dt = DateTime.Now;
@@ -34,7 +31,29 @@ namespace DesktopClock_V2
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+            string PCN = Environment.MachineName;
+            dev1.Text = PCN;
+
             apdatetime();
+            if (PCN.Contains("IKA004"))
+            {
+                try
+                {
+                    string ikapicpath = "C:\\Users\\ika004\\Pictures\\GEKIKAWANENECHANWALLPAPERFORIKACLOCK.png";
+                    Image iimage = Image.FromFile(ikapicpath);
+                    this.BackgroundImage = iimage;
+                }
+                catch (FileNotFoundException)
+                {
+
+                }
+                if (isDebug)
+                {
+                    dev1.Visible = true;
+                    this.Text = this.Text + " -DEBUG";
+                }
+            }
         }
         private void uptime_Tick(object sender, EventArgs e)
         {
@@ -45,6 +64,9 @@ namespace DesktopClock_V2
         {
             this.Close();
         }
+
+
+
 
         private void 壁紙変更CToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -72,7 +94,7 @@ namespace DesktopClock_V2
                     }
                     else
                     {
-                        MessageBox.Show("壁紙を変更できませんでした。\n 画像ファイルの縦横比が9:16ではありません。", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        MessageBox.Show("壁紙を変更できませんでした。\n 画像ファイルの縦横比が9:16ではありません。\n クロップ機能をお試しください。", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     }
                 }
                 catch (Exception ex)
@@ -99,14 +121,92 @@ namespace DesktopClock_V2
                         Image SelectedImage = Image.FromFile(FilePath);
                         string filePath = ofd.FileName;
 
-                        this.BackgroundImage = SelectedImage;
-                        this.BackgroundImageLayout = ImageLayout.Zoom;
-                        MessageBox.Show("壁紙を変更しました");
+                        float aspe = 9f / 16f;
+                        float imgaspe = (float)SelectedImage.Width / SelectedImage.Height;
+                        if (Math.Abs(imgaspe - aspe) < 0.01f)
+                        {
+                            this.BackgroundImage = SelectedImage;
+                            this.BackgroundImageLayout = ImageLayout.Zoom;
+                            MessageBox.Show("壁紙を変更しました");
+                        }
+                        else
+                        {
+                            Image CropIMG = Crop(SelectedImage);
+                            this.BackgroundImage = CropIMG;
+                        }
+
+
                     }
                     catch (Exception ex)
                     {
                         MessageBox.Show($"画像の読み込み中にエラーが発生しました: {ex.Message}");
                     }
+                }
+            }
+        }
+
+
+        public static Image Crop(Image SelectedImage, double targetAspectRatio = 9.0 / 16.0)
+        {
+            Rectangle ClopArea;
+            float imgaspe = (float)SelectedImage.Width / SelectedImage.Height;
+            double targetAspe = 9.0 / 16.0;
+            if (imgaspe > targetAspe)
+            {
+                int NewW = (int)(SelectedImage.Height * targetAspe);
+                int XOffset = (SelectedImage.Width - NewW) / 2;
+                ClopArea = new Rectangle(XOffset, 0, NewW, SelectedImage.Height);
+            }
+            else
+            {
+                int newH = (int)(SelectedImage.Width / targetAspe);
+                int YOffset = (SelectedImage.Height - newH) / 2;
+                ClopArea = new Rectangle(0, YOffset, SelectedImage.Width, newH);
+            }
+
+            Bitmap CroppedIMG = new Bitmap(ClopArea.Width, ClopArea.Height);
+            using (Graphics g = Graphics.FromImage(CroppedIMG))
+            {
+                g.DrawImage(SelectedImage, new Rectangle(0, 0, CroppedIMG.Width, CroppedIMG.Height), ClopArea, GraphicsUnit.Pixel);
+            }
+            return CroppedIMG;
+        }
+
+        private void ChangeWP_crop_Click(object sender, EventArgs e)
+        {
+            //Crop
+
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "Image File(*.bmp,*.jpg,*.png,*.tif)|*.bmp;*.jpg;*.png;*.tif|Bitmap(*.bmp)|*.bmp|Jpeg(*.jpg)|*.jpg|PNG(*.png)|*.png";
+            if (ofd.ShowDialog() == DialogResult.OK)
+
+            {
+                string FilePath = ofd.FileName;
+                try
+                {
+                    Image SelectedImage = Image.FromFile(FilePath);
+                    string filePath = ofd.FileName;
+
+                    float aspe = 9f / 16f;
+                    float imgaspe = (float)SelectedImage.Width / SelectedImage.Height;
+                    if (Math.Abs(imgaspe - aspe) < 0.01f)
+                    {
+                        this.BackgroundImage = SelectedImage;
+                        this.BackgroundImageLayout = ImageLayout.Zoom;
+                        MessageBox.Show("壁紙を変更しました");
+                    }
+                    else
+                    {
+                        Image CropIMG = Crop(SelectedImage);
+                        this.BackgroundImage = CropIMG;
+                        MessageBox.Show("壁紙をクロップして変更しました");
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"画像の読み込み中にエラーが発生しました: {ex.Message}");
                 }
             }
         }
